f051c4bcf7141e7fdeb864f97c7e8efaa90a18de
Kandouci Walid <49974546+WalidKandouci@users.noreply.github.com>
2021-06-16 18:55:16 +0200

    Update modelDefinition.R
    Idea of what/how to plot

--- src/modelDefinition.R
+++ src/modelDefinition.R
@@ -127,7 +127,7 @@ Const             = list(
 ##############################################
 ## Initial (prior to MCMC) parameter values ##
 ##############################################
-previousMCMCfile = here("APT/Jun10-20-00_2021_Temps8.txt")
+previousMCMCfile = here("APT/Jun22-36-34_2021_Temps4.txt")
 previous = read.table(previousMCMCfile, header=TRUE)
 previous = previous[-round(1:nrow(previous)/2),]
 ##
@@ -178,6 +178,21 @@ monitorNodes = rPsyllid$getParents("paras", immediateOnly = TRUE)
 ## detNodes   = detNodes[which(detNodes!="sumLogProb")]
 
 
+#################
+## Plot stages ##
+#################
+
+# The idea I have is to plot at first each stage on it's own, and maybe have the curv of the tree that we want to see in a difrent color ?
+# I'm not sure of what and how to plot because "stage" is a big array and I don't know if we should plot everything ? and if we don't plot everything what should we really plot ?
+par(mfrow=c(4,4))
+for (ii in 1:nstages){
+  for(tt in 1:ntrees){
+    plot(states[tt, ii, substage],type='l', col="grey", xlab="time", ylab="dev")
+    points(states[tt, ii, substage],type='l', col="grey")
+  }
+}
+         
+  
 #############################################################################
 ## See scripts 'cpuTimeExperiment.R' and 'fitModel.R' for using this model ##
 #############################################################################
