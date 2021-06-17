8255174738409abec257126c84d0aeb72429502b
david <david.pleydell@inrae.fr>
2021-06-17 07:07:15 +0200

    Moved Walid's plot code to diagnosticsAndInference.R

--- src/fitModel.R
+++ src/fitModel.R
@@ -151,18 +151,3 @@ if (TRUE) {
 }
 ## class(samples[-c(1:13000),])
 ## crosscorr.plot(as.mcmc(samples[-c(1:13000),]))
-
-
-#################
-## Plot stages ##
-#################
-
-# The idea I have is to plot at first each stage on it's own, and maybe have the curv of the tree that we want to see in a difrent color ?
-# I'm not sure of what and how to plot because "stage" is a big array and I don't know if we should plot everything ? and if we don't plot everything what should we really plot ?
-par(mfrow=c(4,4))
-for (ii in 1:nstages){
-  for(tt in 1:ntrees){
-    plot(states[tt, ii, substage],type='l', col="grey", xlab="time", ylab="dev")
-    points(states[tt, ii, substage],type='l', col="grey")
-  }
-}
