c3db5976d050d678d3ee4c8990cf64284a3cd5b4
david <DRJP>
2021-06-24 10:13:06 +0200

    Alternative queues added in migaleScript1.sh

--- migale/migaleScript1.sh
+++ migale/migaleScript1.sh
@@ -4,6 +4,7 @@ do
     echo $iModel
     # qsub -cwd -N model$iModel -q short.q@@recentNodes -S /bin/sh -V /home/dpleydell/work/CacopsyllaPruni/migale/migaleScript2.sh $iModel
     qsub -cwd -N model$iModel -q long.q@@recentNodes -S /bin/sh -V /home/dpleydell/work/CacopsyllaPruni/migale/migaleScript2.sh $iModel
+    # qsub -cwd -N model$iModel -q infinit.q@@recentNodes -S /bin/sh -V /home/dpleydell/work/CacopsyllaPruni/migale/migaleScript2.sh $iModel
 done
 
 ## long.q    has a 5 day (120 hours) run time limit
