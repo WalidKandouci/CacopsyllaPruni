2e76385ed307e7bbe2b0633de0fa61611854f83c
david <david.pleydell@inrae.fr>
2021-07-10 01:36:29 +0200

    Edits to plotting script.

--- src/diagnosticsAndInference.R
+++ src/diagnosticsAndInference.R
@@ -7,7 +7,7 @@ library(nimbleTempDev)
 ########################
 ## Set some constants ##
 ########################
-SDmodel               = 1 # 1, 2, 3, 4, 5 ## Identifywhich model to use for SD
+SDmodel               = 6 # 1, 2, 3, 4, 5 ## Identifywhich model to use for SD
 nTemps                = 4 # 8 12 16 20 ## Number of temperatures in APT samplers
 thin                  = 10
 nMcmcSamples          = 10 # 1000
@@ -48,6 +48,7 @@ source(here::here("src/modelDefinition.R"))
 ## Compile model to C++ ##
 ##########################
 cPsyllid = compileNimble(rPsyllid)
+# calculate(cPsyllid)
 
 ###############################
 ## List available APT output ##
@@ -75,13 +76,15 @@ samples2 = samples2[-burn,]
 (paraNames = gsub("\\..*","", colnames(samples)) %>% unique()) # Names of parameter nodes
 (depNodes  = gsub("\\[.*","", cPsyllid$getDependencies(paraNames, self = FALSE, includeData = FALSE)) %>% unique())
 
-#####################################################################
-## Plot population stage structure: step 1, construct pStage array ##
-#####################################################################
+#####################################
+## Plot population stage structure ##
+## STEP 1: construct pStage array  ##
+#####################################
 pStage       = array(NA, dim = c(nMcmcSamples, lMeteo, nTrees, nStagesTot)) # This will be a ragged array - i.e. nSteps[tree] is heterogeneous, so some elements of this array will remain as NAs
-devLogMeanSD = array(NA, dim = c(nMcmcSamples, nStagesTot, lTempVec, 2))
-devMean      = array(NA, dim = c(nMcmcSamples, nStagesTot, lTempVec))
-devStdev     = array(NA, dim = c(nMcmcSamples, nStagesTot, lTempVec))
+devLogMeanSD = array(NA, dim = c(nMcmcSamples, nStagesDev, lTempVec, 2))
+devMean      = array(NA, dim = c(nMcmcSamples, nStagesDev, lTempVec))
+devStdev     = array(NA, dim = c(nMcmcSamples, nStagesDev, lTempVec))
+devKernel    = array(NA, dim = c(nMcmcSamples, nStagesDev, lTempVec, res+1))
 for (iMCMC in 1:nMcmcSamples) {
   nimPrint("iMCMC = ", iMCMC)
   iRow    = sample(nrow(samples),1)                          # Index for a random row. Later this can be replaced by a loop.
@@ -100,25 +103,15 @@ for (iMCMC in 1:nMcmcSamples) {
   #######################################################
   for(iTree in 1:nTrees){
     for (iStage in 1:nStagesTot){
-      #plot(cPsyllid$states[iTree, iStage, substage],type='l', col="grey", xlab="time", ylab="dev")
-      #points(cPsyllid$states[iTree, iStage, substage],type='l', col="grey")
-      ## 1) R won't find states. You need to use cPsyllid$states
-      ## 2) states includes all sub-stages. We don't really care about substages, so you need to sum them (in the same way that the model does to obtain pStage
       iMeteo = min(iMeteoForObs[[iTree]]):max(iMeteoForObs[[iTree]])
       nSteps = length(iMeteo)
       if(iStage < nStagesTot){
         pStage[iMCMC,1:nSteps,iTree,iStage] = rowSums(cPsyllid$states[iTree, 1:nSteps, ((iStage-1)*res+1):(iStage*res)])
-      } else{
+      } else {
         pStage[iMCMC,1:nSteps,iTree,iStage] = (cPsyllid$states[iTree, 1:nSteps, ((iStage-1)*res+1)])
       }
-      ## So first construct pStage (the proportion of the population in each stage)
-      ## Then plot it... but also think about how you will handle multiple lines of MCMC output (& will you use lines with transparency or polygons for showing CIs?)
     }
-    #
-    ## if (any(rowSums(pStage[iMCMC,1:nSteps,iTree,])!=1)) {
-    ## print( max(1 - rowSums(pStage[iMCMC,1:nSteps,iTree,])) )
     pStage[iMCMC,1:nSteps,iTree,] = pStage[iMCMC,1:nSteps,iTree,] / rowSums(pStage[iMCMC,1:nSteps,iTree,])
-    ## }
   }
   ####################################################################
   ## Loop on stages to extract mean development at each temperature ##
@@ -127,6 +120,7 @@ for (iMCMC in 1:nMcmcSamples) {
     devLogMeanSD[iMCMC, iStage, 1:lTempVec, 1:2] = t(apply(cPsyllid$paras[iStage,1:lTempVec,1:2], 1, "meanSd2logMeanSd"))
     devMean[iMCMC,iStage,1:lTempVec]  = cPsyllid$paras[iStage,1:lTempVec,1] ## mean  of development kernel
     devStdev[iMCMC,iStage,1:lTempVec] = cPsyllid$paras[iStage,1:lTempVec,2] ## stdev of development kernel
+    devKernel[iMCMC,iStage,1:lTempVec, 1:(res+1)] = cPsyllid$devKernel[iStage,1:lTempVec,1:(res+1)]
   }
 }
 
@@ -150,10 +144,11 @@ for (iMCMC in 1:nMcmcSamples) {
 ##   pStage[iMCMC,1:nSteps,iTree,]
 ## }
 
-########################################################################
-## Plot population stage structure: step 2, plot info in pStage array ##
-########################################################################
-pdf(file = here(paste0("APT/",samplesFileStem, "_proportions.pdf")))
+#######################################
+## Plot population stage structure   ##
+## STEP 2: plot info in pStage array ##
+#######################################
+pdf(file = here(paste0("figures/",samplesFileStem, "_proportions.pdf")))
 for(iTree in 1:nTrees) {
   iMeteo = min(iMeteoForObs[[iTree]]):max(iMeteoForObs[[iTree]])
   nSteps = length(iMeteo)
@@ -191,21 +186,66 @@ dev.off()
 
 
 
+#####################################################
+## Reduce dimension on uncertainty repressentation ##
+## i.e. nMcmcSamples -> nQuantiles                 ##
+#####################################################
+pVec = c(0.01, 0.5, 0.99)
+nQuantiles = length(pVec)
+devKernelQuantiles = array(NA, dim = c(nQuantiles, nStagesDev, lTempVec, res+1))
+devKernelMean      = array(NA, dim = c(nStagesDev, lTempVec, res+1))
+devKernelStdv      = array(NA, dim = c(nStagesDev, lTempVec, res+1))
+for (iStage in 1:nStagesDev) {
+  nimPrint("iStage = ", iStage)
+  for (iTemp in 1:lTempVec) {
+    # Compute quantiles
+    devKernelQuantiles[,iStage,iTemp,] = apply(devKernel[1:nMcmcSamples,iStage,iTemp,1:(res+1)], MARGIN=2, FUN=quantile, p=c(0.01, 0.5, 0.99))
+    devKernelMean[iStage,iTemp,]   = apply(devKernel[1:nMcmcSamples,iStage,iTemp,1:(res+1)], MARGIN=2, FUN=mean)
+    devKernelStdv[iStage,iTemp,]   = apply(devKernel[1:nMcmcSamples,iStage,iTemp,1:(res+1)], MARGIN=2, FUN=sd)
+  }
+}
+
 
 #######################################################################
-## Plot continuous representation of development kernel as quantiles ##
+## Plot discrete representation of development kernel - not pretty!! ##
 #######################################################################
+library(ggplot2)
 
-## devLogMeanSD[iMCMC, iStage, 1:lTempVec, 1:2] =
-##               t(apply(cPsyllid$paras[iStage,1:lTempVec,1:2], 1, "meanSd2logMeanSd"))
-## devMean[iMCMC,iStage,1:lTempVec]  = cPsyllid$paras[iStage,1:lTempVec,1]
-## devStdev[iMCMC,iStage,1:lTempVec] = cPsyllid$paras[iStage,1:lTempVec,2]
+plotList = vector("list", 6)
+for (iStage in 1:6) {
+  meltDevKernMean = reshape::melt(devKernelMean[iStage,,])
+  colnames(meltDevKernMean) = c("Temp","subStage","development")
+  meltDevKernMean$Temp = tempVec[meltDevKernMean$Temp]
+  head(meltDevKernMean)
+  ggp <- ggplot(meltDevKernMean, aes(Temp, subStage)) + geom_tile(aes(fill = development))
+  ## ggp
+  plotList[[iStage]] = ggp + scale_fill_gradient(low = "green", high = "black", breaks=c(0, 0.001, 0.01, 0.1, 1))
+}
+multiplot(plotList[[1]],plotList[[2]],plotList[[3]],plotList[[4]],plotList[[5]],plotList[[6]])
 
 
-cbind(tempVec, devLogMeanSD[iMCMC, iStage, 1:lTempVec, 1:2])
+##########################################################
+## Plot continuous representation of development kernel ##
+##########################################################
+# devLogMeanSD[iMCMC, iStage, 1:lTempVec, 1:2] = t(apply(cPsyllid$paras[iStage,1:lTempVec,1:2], 1, "meanSd2logMeanSd"))
+# devMean[iMCMC,iStage,1:lTempVec]  = cPsyllid$paras[iStage,1:lTempVec,1] ## mean  of development kernel
+# devStdev[iMCMC,iStage,1:lTempVec] = cPsyllid$paras[iStage,1:lTempVec,2] ## stdev of development kernel
 
-xyz = apply(devLogMeanSD[iMCMC, iStage, 1:lTempVec, 1:2], 1, function(x) qlnorm(p=c(0.01, 0.5, 0.99), x[1], x[2]))
-plot(tempVec, xyz[3,], typ="l", ylim=c(0, max(xyz)))
+## Quantiles for each line of MCMC
+devQuantiles  = array(NA, dim = c(nMcmcSamples, nStagesDev, lTempVec, nQuantiles))
+EdevQuantiles = array(NA, dim = c(nStagesDev, lTempVec, nQuantiles))
+for (iStage in 1:nStagesDev)
+  for (iMcmc in 1:nMcmcSamples)
+    devQuantiles[iMcmc,iStage,,] = t(apply(devLogMeanSD[iMCMC, iStage, 1:lTempVec, 1:2], 1, function(x) qlnorm(p=pVec, x[1], x[2])))
+
+## Expected quantiles
+for (iStage in 1:nStagesDev)
+  for (iTemp in 1:nTemps)
+    EdevQuantiles[iStage,iTemp,] = devQuantiles[,iStage,iTemp,] %>% colMeans()
+
+iStage = 3
+iQuant = 3
+plot(tempVec, EdevQuantiles[iStage,,iQuant], typ="l", ylim=c(0, max(EdevQuantiles[iStage,,])))
 lines(tempVec, devMean[iMCMC,iStage,1:lTempVec])
 
 lines(tempVec, xyz[1,])
@@ -217,10 +257,13 @@ summary(rlnorm(n=1E4, -16.43441, 5.398823))
 
 # pdf(file = here(paste0("figures/model",SDmodel,"_devKernelContinuous.pdf")))
 par(mfrow=c(3,2))
-{
-  # Oeuf
+for (iStage in 1:nStagesDev) {
+  for (iQuant in 1:nQuantiles) {
+    image(devKernelQuantiles[iQuant,iStage,,])
 
-  d
+  apply(devKernel[1:nMcmcSamples,iStage,iTemp,1:(res+1)], MARGIN=2, FUN=quantile, p=pVec)
+
+
   devLogMeanSD[iMCMC, iStage, 1:lTempVec, 1:2]
 
   devMean[iMCMC,iStage,1:lTempVec]
@@ -230,36 +273,6 @@ par(mfrow=c(3,2))
   plot(-80:80,meanOeuf,main="Stade oeuf",xlab="température",ylab="devRate",type = "n",xaxs = "i",yaxs = "i")
   polygon(c(-80:80,rev(-80:80)),c((meanOeuf-sdOeuf),rev(meanOeuf+sdOeuf)),col = "springgreen", border = "springgreen",lwd=3)
   lines(-80:80,meanOeuf,lty="solid",col="black",lwd=1.5)
-  axis(1, col = 'black')
-  axis(2, col = 'black')
-  # L1
-  plot(-80:80,meanL1,main="Stade L1",xlab="température",ylab="devRate",type = "n",xaxs = "i",yaxs = "i")
-  polygon(c(-80:80,rev(-80:80)),c((meanL1-sdL1),rev(meanL1+sdL1)),col = "springgreen", border = "springgreen",lwd=3)
-  lines(-80:80,meanL1,lty="solid",col="black",lwd=1.5)
-  axis(1, col = 'black')
-  axis(2, col = 'black')
-  # L2
-  plot(-80:80,meanL2,main="Stade L2",xlab="température",ylab="devRate",type = "n",xaxs = "i",yaxs = "i")
-  polygon(c(-80:80,rev(-80:80)),c((meanL2-sdL2),rev(meanL2+sdL2)),col = "springgreen", border = "springgreen",lwd=3)
-  lines(-80:80,meanL2,lty="solid",col="black",lwd=1.5)
-  axis(1, col = 'black')
-  axis(2, col = 'black')
-  # L3
-  plot(-80:80,meanL3,main="Stade L3",xlab="température",ylab="devRate",type = "n",xaxs = "i",yaxs = "i")
-  polygon(c(-80:80,rev(-80:80)),c((meanL3-sdL3),rev(meanL3+sdL3)),col = "springgreen", border = "springgreen",lwd=3)
-  lines(-80:80,meanL3,lty="solid",col="black",lwd=1.5)
-  axis(1, col = 'black')
-  axis(2, col = 'black')
-  # L4
-  plot(-80:80,meanL4,main="Stade L4",xlab="température",ylab="devRate",type = "n",xaxs = "i",yaxs = "i")
-  polygon(c(-80:80,rev(-80:80)),c((meanL4-sdL4),rev(meanL4+sdL4)),col = "springgreen", border = "springgreen",lwd=3)
-  lines(-80:80,meanL4,lty="solid",col="black",lwd=1.5)
-  axis(1, col = 'black')
-  axis(2, col = 'black')
-  # L5
-  plot(-80:80,meanL5,main="Stade L5",xlab="température",ylab="devRate",type = "n",xaxs = "i",yaxs = "i")
-  polygon(c(-80:80,rev(-80:80)),c((meanL5-sdL5),rev(meanL5+sdL5)),col = "springgreen", border = "springgreen",lwd=3)
-  lines(-80:80,meanL5,lty="solid",col="black",lwd=1.5)
   axis(1, col = 'black')
   axis(2, col = 'black')
 }
